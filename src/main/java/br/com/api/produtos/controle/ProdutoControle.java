7c5347c3048ded1532b1c15251a4d205bd83eafe
vitorramon <rvramon16@gmail.com>
2023-10-07 11:49:32 -0400

    :wrench: chore: Criando controller de produto

--- src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,5 +1,13 @@
 package br.com.api.produtos.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProdutoControle {
-    
+
+    @GetMapping("/")
+    public String rota(){
+        return "Api de produtos";
+    }
 }
--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,5 +1,21 @@
 package br.com.api.produtos.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.repositorio.ProdutoRepositorio;
+
+@Service
 public class ProdutoServico {
     
+    @Autowired
+    private ProdutoRepositorio pr;
+
+    //Método para listar todos os produtos 
+    public Iterable<ProdutoModelo> listar(){
+        return pr.findAll();
+    }
+
+
 }
