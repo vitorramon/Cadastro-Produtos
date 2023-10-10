b46435b3bbe21889d91c53e5019f4e91847b068f
vitorramon <rvramon16@gmail.com>
2023-10-09 20:32:27 -0400

    :sparkles:feat Implementando métodos de cadastrar e listar no controller

--- src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,13 +1,35 @@
 package br.com.api.produtos.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.servico.ProdutoServico;
 
 @RestController
 public class ProdutoControle {
+
+    @Autowired
+    private ProdutoServico ps;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrar(pm);
+    }
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar(){
+        return ps.listar();
+    }
 
     @GetMapping("/")
     public String rota(){
         return "Api de produtos";
     }
+
+
 }
